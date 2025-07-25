29ad501aaeae044558f56ba436ebf91bab307af1
unknown <metaliya.paresh@gmail.com>
2025-07-25 21:33:55 +0530

    Initial project setup with Tailwind CSS v4.1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold  text-midnight flex justify-center items-center ">
+        Welcome to the React project of Data handling...
+      </h1>
+      <p className=" font-bold text-4xl flex align-top text-gray-600">
+        Hello...
+      </p>
+      <h1 className="heading">Try custom</h1>
+    </>
+  );
+}
+
+export default App;
